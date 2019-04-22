0c3d57e3d41a8dd8ab2caf2470d0948d5dbb9874
Alex <expdalex@gmail.com>
2019-04-22 15:51:23 +0300

    это первый комит

---
+++ stage1/project/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+ let a = 7;
+console.log(a);
+/*let person = {
+    name: "Alex",
+    age: 30,
+    isMarried: false
+};
+
+console.log(person.name, person.age);*/
+
+let arr = ["apple.jpg","pineapple.jpg","lemon.jpg"];
+
+console.log(arr[1], arr[2]);
+
+//let answer = +prompt("введи число");
+console.log(typeof(answer));
+
+console.log("3" == 3);
+console.log("5" === 5);
