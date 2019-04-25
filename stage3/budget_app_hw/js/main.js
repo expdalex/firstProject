b6bf174501e62ffc14c01a5e046dae1c8d86a998
Alex <expdalex@gmail.com>
2019-04-25 12:32:02 +0300

    budget_app_hw

---
+++ stage3/budget_app_hw/js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let btnStart = document.getElementById('start');
+let budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value');
+let expensesItem = document.querySelectorAll('.expenses-item');
+let itemBtn = document.getElementsByTagName('button'),
+    expensesItemBtn = itemBtn[0],
+    optionalexpensesBtn = itemBtn[1],
+    countBudgetBtn = itemBtn[2];
+let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+let chooseIncome = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let chooseSum = document.querySelector('.choose-sum');
+let choosePercent = document.querySelector('.choose-percent');
+let yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+    
+console.log(expensesItemBtn);
+console.log(optionalexpensesBtn);
+console.log(countBudgetBtn);
+console.log(chooseSum);
+console.log(choosePercent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
+
+
