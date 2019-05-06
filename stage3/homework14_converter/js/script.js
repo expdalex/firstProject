9f3d8c3c37cf913d917efd855ddbe746cac04c96
Alex <expdalex@gmail.com>
2019-05-06 16:37:56 +0300

    update14

---
+++ stage3/homework14_converter/js/script.js
@@ -0,0 +1,50 @@
+window.addEventListener("DOMContentLoaded", function(){
+    'use strict';
+
+    let inputRub = document.getElementById('rub'),
+        inputUsd = document.getElementById('usd');
+
+        inputRub.addEventListener('input', () => {
+
+            function calc(){
+
+                return new Promise(function(resolve,reject){
+                    let request = new XMLHttpRequest();
+                    request.open('GET', 'js/current.json');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                    request.send();
+                    
+                    /*request.addEventListener('readystatechange', function() {
+                        if(request.readyState === 4) {
+                            if(request.status == 200) {
+                                resolve(request.response);
+                            }
+                            else {
+                                reject();                            
+                            }
+                        }   
+                    });*/
+                    request.addEventListener('load', function() {
+                        if (request.readyState === 4 && request.status == 200) {
+                            resolve(request.response);
+                        } else {
+                            reject();
+                        }
+                    });
+                });
+            }
+       
+
+    calc()
+    .then(response=> {
+                    console.log(response);
+                    let data = JSON.parse(response);
+                    inputUsd.value = inputRub.value / data.usd;
+                })
+    .catch(()=> inputUsd.value = "Что-то пошло не так!");
+
+        });
+
+
+        
+});
